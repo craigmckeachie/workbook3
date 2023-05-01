ca6b1bdc14f20deb1d50ed38940e1ccd79fc90e7
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-01 14:51:46 -0400

    displayAddress

--- exercises/functions/more_functions.js
+++ exercises/functions/more_functions.js
@@ -1,3 +1,19 @@
+function displayMailingAddress(name, streetAddress, city, state, zip) {
+  console.log(`
+        ${name}
+        ${streetAddress}
+        ${city}, ${state} ${zip}
+    `);
+}
+
+let myName = "Craig McKeachie";
+let myStreetAddress = "1016 Center Street";
+let myCity = "Ashland";
+let myState = "OH";
+let myZip = "44805";
+
+displayMailingAddress(myName, myStreetAddress, myCity, myState, myZip);
+
 function addNumbers(num1, num2) {
   let result = num1 + num2;
   let output = `${num1} + ${num2} = ${result}`;
