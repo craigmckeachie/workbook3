2e7190537cc97555b758314b24bb8a7fe2900e3b
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-01 11:40:13 -0400

    simple functions

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,19 @@
+function favoriteThings() {
+  console.log("Craig's favorite things are");
+  console.log("My favorite movie is Usual Suspects.");
+  console.log("My favorite band is A Tribe Called Quest.");
+}
+
+favoriteThings();
+
+function whyImHere() {
+  console.log(
+    `    I really like the students and their attitudes
+     and feel like I'm making a difference in their lives.`
+  );
+}
+
+whyImHere();
+
+
+
