f79e1daa8176048d097fbd457a30603f42643d43
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-02 10:39:44 -0400

    variable hoisting

--- demos/function_scope.js
+++ demos/function_scope.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function increment() {
   let count = 0;
   count = count + 1;
---
+++ demos/variable_hoisting.js
@@ -0,0 +1,9 @@
+
+first = "Ezra"; //assignment
+console.log(first);
+
+// let first; //declaration, NOT hoisted
+var first; //declaration, hoisted (moved to the top of the file or function)
+
+
+
