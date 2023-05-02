04473582e207a4c8234abb27182347af4102fb65
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-02 10:16:41 -0400

    scope demos

---
+++ demos/function_scope.js
@@ -0,0 +1,8 @@
+function increment() {
+  let count = 0;
+  count = count + 1;
+  return count;
+}
+
+let count1 = increment();
+console.log(count1);
---
+++ demos/global_scope.js
@@ -0,0 +1,8 @@
+let count = 0;
+
+function increment() {
+  count = count + 1;
+}
+
+increment();
+console.log(count);
