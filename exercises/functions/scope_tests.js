4d3121c6b199cb0f2952f95494ecf369c956ded8
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-02 12:50:48 -0400

    example 5 explained

---
+++ exercises/functions/scope_tests.js
@@ -0,0 +1,20 @@
+"use strict";
+let a = 4; //global a
+
+function test5() {
+  a = 7; //global a
+  function again() {
+    let a = 8; //local a
+    console.log("a = " + a); //local
+  }
+
+  again();
+  console.log("a = " + a); //global a
+}
+
+test5();
+console.log("a = " + a); //global a
+
+// a= 8
+// a = 7
+// a = 7
