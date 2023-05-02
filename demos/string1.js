c62f6f95a2c33df70cf4c4db45ecdad39f9394e0
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-02 13:32:46 -0400

    string examples

---
+++ demos/string1.js
@@ -0,0 +1,15 @@
+let quote1 = "In Texas, we use the word ya'll often";
+console.log(quote1);
+
+let quote2 = 'Big Tex (at the State Fair) says "Howdy"';
+console.log(quote2);
+
+
+let quote1a = 'In Texas, we use the word ya\'ll often';
+console.log(quote1a);
+let quote2a = "Big Tex (at the State Fair) says \"Howdy\"";
+console.log(quote2a);
+
+let name = "Mark Westly";
+let nameLength = name.length;
+console.log(name + " is " + nameLength + " characters long");
