42e9282545291a7f60006f22c849fb32126abdae
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-02 09:59:27 -0400

    mo review

--- demos/review1.js
+++ demos/review1.js
@@ -1,11 +1,12 @@
 "use strict";
 //greeting takes a name parameter
-function greeting(first){
-    return `Hello ${first}`
+function greeting(first, last){
+    return `Hello ${first} ${last}`
 }
 
 let firstName = "Will";
+let lastName = "Roberson"
 // calling/invoking greeting function and passing the firstName argument
 //catching and assigning the return value of the function into the message variable
-let message = greeting(firstName); 
+let message = greeting(firstName, lastName); 
 console.log(message);
