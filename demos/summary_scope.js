d93bcdc360e6a7e977cb8d6caf9b357803f74c8d
Craig McKeachie <craigdmckeachie@gmail.com>
2023-05-02 11:05:21 -0400

    scope summary

--- demos/summary_scope.js
+++ demos/summary_scope.js
@@ -1,20 +1,24 @@
-var globalVariable = 'global';
+let globalVariable = "global";
 
 function myFirstFunction() {
-    let functionVariable = 'function var';
-    console.log(`Global ${globalVariable}   `);
-    console.log(`Function ${functionVariable}`);
-    // console.log(`Function ${secondFunctionVariable}`); //not defined
+  let functionVariable = "function var";
+  console.log(`Global ${globalVariable}   `);
+  console.log(`Function ${functionVariable}`);
+  // console.log(`Function ${secondFunctionVariable}`); //not defined
+
+  if (true) {
+    let blockVariable = "block var";
+    console.log(`Block ${blockVariable}`);
+  }
+  //   console.log(`Block outside of block ${blockVariable}`); //not defined
 }
 
 function mySecondFunction() {
-    let secondFunctionVariable = 'second function var';
-    console.log(`Global ${globalVariable}   `);
-    // console.log(`Function ${functionVariable}`); //not defined
-    console.log(`Function ${secondFunctionVariable}`);
-
+  let secondFunctionVariable = "second function var";
+  console.log(`Global ${globalVariable}   `);
+  // console.log(`Function ${functionVariable}`); //not defined
+  console.log(`Function ${secondFunctionVariable}`);
 }
-
 
 myFirstFunction();
 mySecondFunction();
