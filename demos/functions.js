2aa5036975e8d365a625d7e67efbc23e3d315d48
Craig McKeachie <craigdmckeachie@gmail.com>
2023-06-01 15:03:31 -0400

    add examples

---
+++ demos/functions.js
@@ -0,0 +1,14 @@
+// let s = "a string";
+// let n = 123;
+
+let myConsole = {
+  s: "a string",
+  n: 1,
+  log: function (thing) {
+    console.log(thing);
+  },
+};
+
+let myString = myConsole.s;
+let myNumber = myConsole.n;
+myConsole.log("Zanaya");
---
+++ demos/functions2.js
@@ -0,0 +1,54 @@
+// let thing = "dance"
+
+function doSomething(one, two, three) {
+  return "do that " + one + two + three;
+}
+
+let a = 1;
+let b = 2;
+let c = 3;
+let returnValue = doSomething(a, b, c);
+debugger;
+
+// let s = "a string";
+{
+  /* <p>This is the stuff...</p>; */
+}
+
+// let Person = {
+//   first: "",
+//   last: "",
+//   pet: "",
+//   petType: "",
+//   petBreed: "",
+//   city: "",
+// };
+
+// let Element = {
+//     innerText: "",
+//     id: "",
+//     name: ""
+// }
+
+// let zanaya = {
+//   first: "Zanaya",
+//   last: "Austin",
+//   pet: "King",
+//   petType: "Dog",
+//   petBreed: "Husky",
+//   city: "Pittsburg",
+// };
+
+// let sasha = {
+//   first: "Sasha",
+//   last: "McBride",
+//   pet: "King",
+//   petType: "Dog",
+//   petBreed: "Husky",
+//   city: "Pittsburg",
+// };
+
+// let messageParagraph = document.querySelector("#message-paragraph");
+// let messageParagraph = {
+//   innerText: "This is the stuff...",
+// };
